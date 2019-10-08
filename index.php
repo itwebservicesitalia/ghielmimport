8c1287f8ab16fe4348736517a994863b679d7b70
Riccardo Sacco <rickybag99@gmail.com>
2019-10-08 16:12:55 +0200

    added multilanguage support

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Richiesta d'offerta</title>
-    <meta http-equiv="refresh" content="0;URL=ferro.php">
+    <meta http-equiv="refresh" content="0;URL=ferro">
 </head>
 <body>
     
