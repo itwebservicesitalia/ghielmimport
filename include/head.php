faf2666469ecbc09a0ed3f60e08b3fdfa2e00072
rickybag <rickybag99@gmail.com>
2019-11-28 17:50:32 +0100

    added monitoring

--- include/head.php
+++ include/head.php
@@ -36,3 +36,13 @@
 
     <link rel="stylesheet" href="assets/css/style.css" />
     <link rel="stylesheet" href="assets/css/sidebar.css" />
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-153526853-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-153526853-1');
+  </script>
